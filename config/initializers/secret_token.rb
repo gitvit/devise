71dccc5e63dcc2a84400e60d7c8d4a010f49a31e
gitvit <git_vit@ukr.net>
2015-06-15 15:00:26 +0300

    first_git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'f9e14b5b4cf60c87a4a775a15a872910ca28b0e5389d3b4ae46fff8fbee60b6746ab263dad503727f05ef758a0b90d3d0fdfc2e25e2571492183685172520909'
